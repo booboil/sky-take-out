18c0f0ad3a52af9a271e8b51b4834b419ccb26b0
张新荣 <2914424377@qq.com>
2023-12-23 16:41:56 +0800

    菜品管理-菜品批量删除功能

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -12,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/admin/dish")
 @Api(tags = "菜品相关接口")
@@ -41,5 +43,14 @@ public class DishController {
         PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
         return Result.success(pageResult);
     }
+
+    @GetMapping("/delete")
+    @ApiOperation("菜品批量删除")
+    public Result delete(@RequestParam List<Long> ids) { // @RequestParam注解动态解析字符串
+        log.info("菜品分页查询:{}",ids);
+        dishService.deleteBatch(ids);
+        return Result.success();
+    }
+
 
 }
