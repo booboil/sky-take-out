d83ef35351daa5ee1b39db87277d23da2970d779
张新荣 <2914424377@qq.com>
2024-01-28 21:42:37 +0800

    HttpClient发送POST方式请求

--- sky-server/src/test/java/com/sky/HttpClientTest.java
+++ sky-server/src/test/java/com/sky/HttpClientTest.java
@@ -1,15 +1,20 @@
 package com.sky;
 
 import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 
 @SpringBootTest
 public class HttpClientTest {
@@ -25,7 +30,7 @@ public class HttpClientTest {
         // 2.创建请求对象
         HttpGet httpGet = new HttpGet("http://localhost:8080/user/shop/status");
 
-        // 3.发送请求,接受响应结果
+        // 3.发送请求,接收响应结果
         CloseableHttpResponse response = httpClient.execute(httpGet);
 
         // 4.获取服务端返回的状态码
@@ -45,4 +50,39 @@ public class HttpClientTest {
     /**
      * 测试HttpClient发送POST方式请求
      */
+    @Test
+    public void testPOST() throws Exception {
+        // 1.创建httpClient对象
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        // 2.创建请求对象
+        HttpPost httpPost = new HttpPost("http://localhost:8080/admin/employee/login");
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("username","admin");
+        jsonObject.put("password","123456");
+
+        StringEntity entity = new StringEntity(jsonObject.toString());
+        // 指定请求编码方式
+        entity.setContentEncoding("utf-8");
+        // 数据格式 （Json格式）
+        entity.setContentType("application/json");
+        httpPost.setEntity(entity);
+
+        // 3.发送请求
+        CloseableHttpResponse response = httpClient.execute(httpPost);
+
+        // 4.解析返回结果
+        int statusCode = response.getStatusLine().getStatusCode();
+        System.out.println("响应状态码为：" + statusCode);
+        // 具体响应数据
+        HttpEntity entity1 = response.getEntity();
+        String body = EntityUtils.toString(entity1);
+        System.out.println("响应数据为：" + body);
+
+        // 5.关闭资源
+        response.close();
+        httpClient.close();
+    }
+
 }
